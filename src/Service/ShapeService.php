bb12a105ae03c083ff3ef54a9dd6f48221cebf21
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <phtfao@gmail.com>
2022-07-26 03:14:00 -0300

    Creates methods to sum areas and perimeters of two shapes.

--- src/Service/ShapeService.php
+++ src/Service/ShapeService.php
@@ -25,4 +25,14 @@ class ShapeService
     {
         return $this->shape->getInfo();
     }
+
+    public static function getSumOfAreas(ShapeInterface $shape1, ShapeInterface $shape2)
+    {
+        return $shape1->getArea() + $shape2->getArea();
+    }
+
+    public static function getSumOfPerimeters(ShapeInterface $shape1, ShapeInterface $shape2)
+    {
+        return $shape1->getPerimeter() + $shape2->getPerimeter();
+    }
 }
